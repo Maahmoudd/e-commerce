a4d7d9b33829ebe2bd07d01d271eaf7dcadf0d9a
Maahmoudd <mahmoudmuhammed2610@gmail.com>
2024-01-25 12:27:24 +0200

    Use image upload trait for profile image update

--- app/Http/Services/Backend/ProfileService.php
+++ app/Http/Services/Backend/ProfileService.php
@@ -2,29 +2,24 @@
 
 namespace App\Http\Services\Backend;
 
+use App\Traits\ImageUploadTrait;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
 
 class ProfileService
 {
+    use ImageUploadTrait;
 
     public function profileUpdate($request)
     {
-
         $user = Auth::user();
 
         if ($request->hasFile('image')){
             if (File::exists(public_path($user->image))){
                 File::delete(public_path($user->image));
             }
-            $image = $request->image;
-            $imageName = rand().'_'.$image->getClientOriginalName();
-            $image->move(public_path('uploads'), $imageName);
-
-            $path = "/uploads/".$imageName;
-            $user->image = $path;
+            $request['image'] = $this->uploadImage($request, 'image', 'uploads');
         }
-
         $user->name = $request->name;
         $user->email = $request->email;
         $user->save();

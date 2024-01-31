5399c6a66650639d366a1dc8166bc4a70dbc98e3
Maahmoudd <mahmoudmuhammed2610@gmail.com>
2024-01-31 08:44:54 +0200

    Render child categories on child category index page

--- app/Models/ChildCategory.php
+++ app/Models/ChildCategory.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ChildCategory extends Model
 {
@@ -16,4 +17,13 @@ class ChildCategory extends Model
         'status',
         'slug',
     ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+    public function subCategory(): BelongsTo
+    {
+        return $this->belongsTo(SubCategory::class);
+    }
 }
